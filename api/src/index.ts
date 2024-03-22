5e24dc06b47314cf9be8e14303dc6bbb2a22bd07
Omar Reyes Zamudio <72781778+omar49511@users.noreply.github.com>
2024-03-22 02:54:14 -0700

    inicio del api rest

---
+++ api/src/index.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
